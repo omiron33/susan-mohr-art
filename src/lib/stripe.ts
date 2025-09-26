8d93934ff125244cd537d298e95f4db89dee53e2
Shane Fisher <sjfisher33@gmail.com>
2025-09-26 00:29:56 -0500

    fix: Update Stripe API version to latest supported version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -7,7 +7,7 @@ export function getStripeClient() {
   if (!stripeClient) {
     const apiKey = getRequiredEnv("STRIPE_SECRET_KEY");
     stripeClient = new Stripe(apiKey, {
-      apiVersion: "2024-06-20",
+      apiVersion: "2025-08-27.basil",
     });
   }
   return stripeClient;
